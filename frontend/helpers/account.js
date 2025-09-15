091303d150caf6d338ce913c636fbb4ff3a3765d
Khoa Tran <s4136776@rmit.edu.vn>
2025-09-15 21:20:47 +0700

    fix config

--- frontend/helpers/account.js
+++ frontend/helpers/account.js
@@ -1,53 +1,9 @@
-import { fetchHelper } from './fetch';
-
 const helper = {};
 
 helper.role = /** @type {const} */{
 	VENDOR: 'Vendor',
 	SHIPPER: 'Shipper',
 	CUSTOMER: 'Customer',
-};
-
-/**
- * @typedef {{ success: boolean, error: Record<string, string[]> }} SignUpState
- * @param {SignUpState} _
- * @param {FormData} formData
- * @returns {Promise<SignUpState>}
- */
-helper.signUp = async (_, formData) => {
-	try {
-		/** @type {app.Response} */
-		const res = await fetchHelper.postForm('/api/signup', formData).json();
-		await new Promise(resolve => setTimeout(resolve, 400));
-		if (res.success) {
-			return { success: true, error: {} };
-		}
-		return { success: false, error: res.error };
-	} catch (e) {
-		console.error(e);
-		return { success: false, error: { __global: ['[Network] Unable to signup!'] } };
-	}
-};
-
-/**
- * @typedef {{ success: boolean, error: Record<string, string[]> }} UpdateProfilePictureState
- * @param {UpdateProfilePictureState} _
- * @param {FormData} formData
- * @returns {Promise<UpdateProfilePictureState>}
- */
-helper.updateProfilePicture = async (_, formData) => {
-	try {
-		/** @type {app.Response} */
-		const res = await fetchHelper.postForm('/api/account/profile-picture', formData).json();
-		await new Promise(resolve => setTimeout(resolve, 400));
-		if (res.success) {
-			return { success: true, error: {} };
-		}
-		return { success: false, error: res.error };
-	} catch (e) {
-		console.error(e);
-		return { success: false, error: { __global: ['[Network] Unable to update profile picture!'] } };
-	}
 };
 
 export const accountHelper = helper;
