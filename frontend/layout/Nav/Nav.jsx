ea5361c4e4affde7352544a540ffdcf3410210e2
ddnguyen99 <dacnguyen2101@gmail.com>
2025-09-16 20:30:52 +0700

    Icon + Favicon

--- frontend/layout/Nav/Nav.jsx
+++ frontend/layout/Nav/Nav.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router';
 
+import shopIcon from '../../../public/rmit-icon.png';
+
 import { NavItems } from './NavItems/NavItems';
 
 export const Nav = () => (<>
 	<nav className='layout__nav navbar navbar-expand d-none d-md-flex shadow'>
 		<div className='container-fluid'>
-			<Link className='navbar-brand' to='/'>
-				🛒 Shop
+			<Link className='navbar-brand d-flex align-items-center gap-2' to='/'>
+				<img src={shopIcon} alt='Shop icon' width='28' height='28' />
+				<span>Shop</span>
 			</Link>
 			<ul className='navbar-nav'>
 				<NavItems />
@@ -16,16 +19,18 @@ export const Nav = () => (<>
 
 	<nav className='layout__nav navbar d-md-none shadow'>
 		<div className='container-fluid'>
-			<Link className='navbar-brand' to='/'>
-				🛒 Shop
+			<Link className='navbar-brand d-flex align-items-center gap-2' to='/'>
+				<img src={shopIcon} alt='Shop icon' width='24' height='24' />
+				<span>Shop</span>
 			</Link>
 			<button className='navbar-toggler' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasNavbar' data-bs-theme='dark' aria-controls='offcanvasNavbar' aria-label='Toggle navigation'>
 				<span className='navbar-toggler-icon'></span>
 			</button>
 			<div className='offcanvas offcanvas-end' tabIndex={-1} id='offcanvasNavbar' aria-labelledby='offcanvasNavbarLabel'>
 				<div className='offcanvas-header'>
-					<h5 className='offcanvas-title' id='offcanvasNavbarLabel'>
-						🛒 Shop
+					<h5 className='offcanvas-title d-flex align-items-center gap-2' id='offcanvasNavbarLabel'>
+						<img src={shopIcon} alt='Shop icon' width='24' height='24' />
+						<span>Shop</span>
 					</h5>
 					<button type='button' className='btn-close' data-bs-dismiss='offcanvas' aria-label='Close'></button>
 				</div>
